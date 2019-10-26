0cc99d13f49391b5454d043438396adb3b28c088
davidcastellanos <castellanos.johann@gmail.com>
2019-10-26 15:14:29 -0500

    [Feature] Added new files

--- holberton.h
+++ holberton.h
@@ -1,4 +1,20 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+
+
+
 int _printf(const char *format, ...);
+
 #endif /*_HOLBERTON_H_*/
